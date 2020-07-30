5ecc1f9e78d8ce75e67876d168c85ff9ea723f94
Rai Medeiros <raismedeiros@gmail.com>
2020-07-30 19:48:49 -0300

    setting up styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  *{
+    padding:0;
+    margin:0;
+    outline:0;
+    box-sizing:border-box;
+  }
+  body{
+    background:#F0F0F5 url(${githubBackground}) 70% top no-repeat;
+    -webkit-font-smoothing: antialiased;
+  }
+  body,input,button{
+    font:16px Roboto, sans-serif;
+  }
+  #root{
+    max-width:9 60px;
+    padding:40px 20px;
+    margin:0 auto;
+  }
+  button{
+    cursor:pointer;
+  }
+`;
